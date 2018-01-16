d5df46ee7e8d0bd12e439ab9b60d381877935866
意无瑕 <740393778@qq.com>
2018-01-16 09:07:43 +0800

    Add files via upload

---
+++ src/main/java/com/baomidou/springmvc/controller/UserController.java
@@ -0,0 +1,79 @@
+package com.baomidou.springmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.springmvc.model.enums.TypeEnum;
+import com.baomidou.springmvc.model.system.User;
+import com.baomidou.springmvc.service.system.IUserService;
+
+/**
+ * Author: D.Yang
+ * Email: koyangslash@gmail.com
+ * Date: 16/10/9
+ * Time: 上午11:58
+ * Describe: 用户控制器
+ * 
+ * 代码生成器，参考源码测试用例：
+ * 
+ * /mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/generator/MysqlGenerator.java
+ *
+ */
+@Controller
+public class UserController extends BaseController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+    	//SpringServletContainerInitializer a=null;
+        modelAndView.setViewName("index");
+        //new Page<User>(1, 10),
+        Page<User> b =new Page<User>(1, 4);
+        Wrapper<User> a = new EntityWrapper<User>().eq("name", "张三");
+        Page<User> ss= userService.selectPage(b);
+        modelAndView.addObject("userList", userService.selectPage(b).getRecords());
+        return modelAndView;
+    }
+
+    @RequestMapping("/preSave")
+    public ModelAndView preSave(ModelAndView modelAndView, @RequestParam(value = "id", required = false) Long id) {
+        modelAndView.setViewName("save");
+        if (id != null) {
+            modelAndView.addObject("user", userService.selectById(id));
+        }
+        return modelAndView;
+    }
+
+    @ResponseBody
+    @RequestMapping("save")
+    public Object save(User user) {
+        user.setType(TypeEnum.DISABLED);
+        if (user.getId() == null) {
+            return userService.insert(user) ? renderSuccess("添加成功") : renderError("添加失败");
+        } else {
+            return userService.updateById(user) ? renderSuccess("修改成功") : renderError("修改失败");
+        }
+    }
+
+    @ResponseBody
+    @RequestMapping("/delete")
+    public Object delete(@RequestParam(value = "id", required = false) Long id) {
+    	Integer dInteger=3;
+        return userService.deleteById(id) ? renderSuccess("删除成功") : renderError("删除失败");
+    }
+    
+    
+}
